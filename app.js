1895fce69fab4785a230d7ecd72422b2c861f91c
zippiespick <zippiespick@gmail.com>
2021-02-22 15:40:28 -0500

    Modified package.json file

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ app.use(express.static('public'));
 //make way for some custome css, js, and images
 app.use('/css', express.static(__dirname + '/public/css'));
 app.use('/js', express.static(__dirname + '/public/js'));
-app.use('/img', express.static(__dirname + '/public/img'));
-app.use('/videos', express.static(__dirname + '/public/videos'));
 
 var server = app.listen(process.env.PORT || 8080, function() {
     var port = server.address().port;
